2b98f3f933a9dc75710a86b0298a67ae59b3854f
SiameseChen <arthurchenevans@gmail.com>
2022-05-05 18:14:59 +0700

    Now accepts user input to play rock-paper-scissors.

--- javascript/rockpaperscissors.js
+++ javascript/rockpaperscissors.js
@@ -1,17 +1,45 @@
+// Gets random integer
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+// PC random throw
 function computerPlay() {
     let randNum = getRandomInt(3);
     let computerHand;
     if (randNum == 0) {
-        computerHand = 'rock'
+        computerHand = 'rock';
     } else if (randNum == 1) {
         computerHand = 'paper'
     } else if  (randNum == 2) {
-        computerHand = 'scissors'
+        computerHand = 'scissors';
     }
-    console.log(computerHand)
+    return computerHand;
 }
-computerPlay();
+
+// Function that plays one round
+function rpsRound(playerSelection, computerSelection) {
+    playerSelection = prompt("What will you throw? \n");
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerPlay();
+
+    if (playerSelection == computerSelection) {
+        console.log(`Player has picked: ${playerSelection}, Computer has picked: ${computerSelection}. \n Result: Draw!`);
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        console.log(`Player has picked: ${playerSelection}, Computer has picked: ${computerSelection}. \n Result: Computer wins!`);
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors'){
+        console.log(`Player has picked: ${playerSelection}, Computer has picked: ${computerSelection}. \n Result: Player wins!`);
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        console.log(`Player has picked: ${playerSelection}, Computer has picked: ${computerSelection}. \n Result: Computer wins!`);
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        console.log(`Player has picked: ${playerSelection}, Computer has picked: ${computerSelection}. \n Result: Player wins!`);
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        console.log(`Player has picked: ${playerSelection}, Computer has picked: ${computerSelection}. \n Result: Computer wins!`);
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        console.log(`Player has picked: ${playerSelection}, Computer has picked: ${computerSelection}. \n Result: Player wins!`);
+    } else {
+        console.log(`${playerSelection} invalid. Please type \'rock\', \'paper\', or \'scissors\'.`)
+    }
+
+}
+rpsRound();
